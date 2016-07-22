d399457df1eb4878b0b2ce0ec261e87d3ec1c45f
YaqiMiao <yaqimiao@berkeley.edu>
2016-07-22 11:36:25 +0800

    simple hello world

--- README.rst
+++ README.rst
@@ -1,6 +1,4 @@
-Sample Module Repository
+GTK Project Examples
 ========================
 
-This simple project is an example repo for Python projects.
-
-`Learn more <http://www.kennethreitz.org/essays/repository-structure-and-python>`_.
+Experimenting with some gtk+ functions.
--- sample/core.py
+++ sample/core.py
@@ -1,9 +1,25 @@
 # -*- coding: utf-8 -*-
 
-def get_hmm():
-    """Get a thought."""
-    return 'hmmm...'
+import sys
 
-def hmm():
-    """Contemplation..."""
-    print get_hmm()
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+
+class MyWindow(Gtk.Window):
+
+    def __init__(self):
+        Gtk.Window.__init__(self, title="Hello World")
+
+        self.button = Gtk.Button(label="Click Here")
+        self.button.connect("clicked", self.on_button_clicked)
+        self.add(self.button)
+
+    def on_button_clicked(self, widget):
+        print("Hello World")
+	sys.stdout.flush()
+
+win = MyWindow()
+win.connect("delete-event", Gtk.main_quit)
+win.show_all()
+Gtk.main()
